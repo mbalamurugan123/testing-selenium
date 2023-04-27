a2ae2b9cec3ddac3eafc1954f54f98fe7c063b26
BALAMURUGAN M <100469671+mbalamurugan123@users.noreply.github.com>
2023-04-27 15:47:24 +0530

    Update App.java

--- day05/src/main/java/test_01/day05/App.java
+++ day05/src/main/java/test_01/day05/App.java
@@ -1,23 +1,39 @@
 package test_01.day05;
 
+package com.example.demo.Testing;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
-    	 WebDriverManager.firefoxdriver().setup();
-         WebDriver driver=new FirefoxDriver();
-         driver.get("https://demoqa.com/droppable/");
-         driver.findElement(By.xpath("//*[@id=\\\"draggable\\\"]"));
-         driver.findElement(By.xpath("//*[@id=\\\"droppable\\\"]"));
-         
-         Actions act=new Actions(driver);
-         
+    	 WebDriverManager.edgedriver().setup();
+         WebDriver driver=new EdgeDriver();
+         driver.get("https://demo.opencart.com/index.php?route=account/register&language=en-gb");
+         //maximize the window screen
+         driver.manage().window().maximize();
+         //Navigating down
+         JavascriptExecutor js =  (JavascriptExecutor) driver;
+         js.executeScript("window.scrollBy(0,800)", args);
+         //find the first name
+         driver.findElement(By.xpath("/html/body/main/div[2]/div/div/form/fieldset[1]/div[2]/div/input")).sendKeys("Bala");
+         driver.findElement(By.xpath("/html/body/main/div[2]/div/div/form/fieldset[1]/div[3]/div/input")).sendKeys("Murugan");
+         driver.findElement(By.xpath("/html/body/main/div[2]/div/div/form/fieldset[1]/div[4]/div/input")).sendKeys("727721euai011@gmail.com");
+         driver.findElement(By.xpath("/html/body/main/div[2]/div/div/form/fieldset[2]/div/div/input")).sendKeys("Bala@123");
+        
+         driver.findElement(By.xpath("//*[@id=\"form-register\"]/fieldset[3]/div/div/div[1]/label")).click();
+         Thread.sleep(5000);
+         driver.findElement(By.xpath("//*[@id=\"form-register\"]/div/div/div/input")).click();
+         driver.findElement(By.xpath("//*[@id=\"form-register\"]/div/div/button")).click();
+         Thread.sleep(5000);
+    
+     
+ 
     }
 }
